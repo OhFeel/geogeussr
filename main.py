fa8655039e8a4f60a89f324c39f212a23b72fd9f
OhFeel <76810604+OhFeel@users.noreply.github.com>
2022-09-29 17:24:54 +0200

    v1.2
    cleaner code // option to use custom url

--- main.py
+++ main.py
@@ -32,19 +32,35 @@ import pyautogui
 
 
 
-print(f"{Fore.WHITE}[{Fore.RED}Console{Fore.WHITE}] How many times do you want to run the script?")
-num2 = int(input())
-print(f"{Fore.WHITE}[{Fore.RED}Console{Fore.WHITE}] Script will run {num2} times")
-print(f"{Fore.WHITE}[{Fore.RED}Console{Fore.WHITE}] Please follow every step carefully")
-print(f"{Fore.WHITE}[{Fore.RED}Console{Fore.WHITE}] Opening Browser...")
-
-webbrowser.open('https://www.geoguessr.com/maps/623b4eed21380468d68ca6e6')
 
 
 class geoguessr:
 
   def __init__(self):
         self.delay = 1.5
+        
+  def get_url(self):
+    print(f"{Fore.WHITE}[{Fore.RED}Console{Fore.WHITE}] How many times do you want to run the script?")
+    self.num2 = int(input())
+    print(f"{Fore.WHITE}[{Fore.RED}Console{Fore.WHITE}] Script will run {self.num2} times")
+    print(f"{Fore.WHITE}[{Fore.RED}Console{Fore.WHITE}] Please follow every step carefully")
+
+    print(f"{Fore.WHITE}[{Fore.RED}Console{Fore.WHITE}] Do you want a custom url or the default one?")
+    print(f"{Fore.WHITE}[{Fore.RED}Console{Fore.WHITE}] 1. Default")
+    print(f"{Fore.WHITE}[{Fore.RED}Console{Fore.WHITE}] 2. Custom")
+    url = int(input())
+    if url == 1:
+      get_url = 'https://www.geoguessr.com/maps/623b4eed21380468d68ca6e6'
+
+    if url == 2:
+      print(f"{Fore.WHITE}[{Fore.RED}Console{Fore.WHITE}] Please enter the url")
+      get_url = input()
+      print(f"{Fore.WHITE}[{Fore.RED}Console{Fore.WHITE}] Url set to {get_url}")
+
+    time.sleep(1)
+    print(f"{Fore.WHITE}[{Fore.RED}Console{Fore.WHITE}] Opening Browser...")
+
+    webbrowser.open(get_url)
 
 
   def get_positions(self):
@@ -127,12 +143,12 @@ class geoguessr:
         break
     self.print_console("Done!")
     self.print_console("Setup is complete, the script will run automatically in 5 seconds")
-    self.print_console(f"Estimated time: {num2 * 20} Seconds")
+    self.print_console(f"Estimated time: {self.num2 * 20} Seconds")
 
 
   def run(self):
     
-    for x in range(num2): 
+    for x in range(self.num2): 
       time.sleep(self.delay)
       pyautogui.moveTo(self.pos_play)
       pyautogui.click()
@@ -187,12 +203,20 @@ class geoguessr:
 
   def fast_slow(self):
     self.print_console("Do you have fast or slow internet?")
-    self.print_console("option: 1 = slow 2 = fast 3 = superslow")
+    self.print_console("1. Fast")
+    self.print_console("2. Medium")
+    self.print_console("3. Slow")
+    self.print_console("4. Super Slow")
+
     num_fast_slow = int(input())
-    if num_fast_slow == 1:
-      self.delay = 5
     if num_fast_slow == 3:
-      self.delay = 7.5  
+      self.delay = 5
+    if num_fast_slow == 4:
+      self.delay = 7.5 
+    if num_fast_slow == 2:
+      self.delay = 2.5
+
+
 
 
 
@@ -200,7 +224,8 @@ class geoguessr:
         print(f"{Fore.WHITE}[{Fore.RED}{status}{Fore.WHITE}] {arg}")
   def main(self):
     
-    os.system(f"title" + " Geoguessr Bot // @ohfeel on github // v 1.1")
+    os.system(f"title" + " Geoguessr Bot // @ohfeel on github // v 1.2")
+    geoguessr.get_url(self)
     geoguessr.fast_slow(self)
     geoguessr.get_positions(self)
     geoguessr.run(self)
